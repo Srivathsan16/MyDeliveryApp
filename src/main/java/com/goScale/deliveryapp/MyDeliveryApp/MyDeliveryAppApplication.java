824070fa2e5d32a8deaec06856c2063c7f114073
Srivathsan <srivathsanrajagopal@gmail.com>
2018-10-01 22:09:49 +0530

    first commit

---
+++ src/main/java/com/goScale/deliveryapp/MyDeliveryApp/MyDeliveryAppApplication.java
@@ -0,0 +1,12 @@
+package com.goScale.deliveryapp.MyDeliveryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDeliveryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDeliveryAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/goScale/deliveryapp/MyDeliveryApp/MyDeliveryAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.goScale.deliveryapp.MyDeliveryApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyDeliveryAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
